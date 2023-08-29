bd62de4c6a958130901749315e8d3fb8250bd75d
SAMREEN AKHTAR <110802942+samreen-akhtar@users.noreply.github.com>
2023-08-29 11:09:37 +0530

    Update navbar.js

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,11 +1,7 @@
 import "./navbar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faUser,
-  faMagnifyingGlass,
-  faBars,
-} from "@fortawesome/free-solid-svg-icons";
-const Navbar = ({ sidebarOpen, openSidebar }) => {
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar__left">
