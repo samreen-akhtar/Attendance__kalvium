1d2d347d9f70d2ece83ee6c8ff0809ba3e4d1b21
SAMREEN AKHTAR <110802942+samreen-akhtar@users.noreply.github.com>
2023-08-29 11:06:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import { useState } from "react";
 import Navbar from "./components/navbar/navbar";
 import Sidebar from "./components/sidebar/sidebar";
 import Main from "./components/main/main";
 const App = () => {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-  const openSidebar = () => {
-    setSidebarOpen(true);
-  };
-  const closeSidebar = () => {
-    setSidebarOpen(false);
-  };
   return (
     <div className="container">
-      <Navbar sidebarOpen={sidebarOpen} openSidebar={openSidebar} />
+      <Navbar />
       <h2>School Portal</h2>
-      <Sidebar sidebarOpen={sidebarOpen} closeSidebar={closeSidebar} />
+      <Sidebar />
       <Main />
     </div>
   );
